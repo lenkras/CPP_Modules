b029c83aa005e353f69427cb0533d374114f25f2
Elena <yelenakras@gmail.com>
2024-11-12 19:17:04 +0200

    add ex00 in Module5

---
+++ CPP_M05/ex00/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epolkhov <epolkhov@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-08 14:15:17 by epolkhov          #+#    #+#             */
+/*   Updated: 2024-11-08 14:15:17 by epolkhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string &name, int grade);
+		Bureaucrat(const Bureaucrat& copy);
+		Bureaucrat& operator=(const Bureaucrat& copy);
+		~Bureaucrat();
+
+		const std::string &getName()const;
+		int getGrade()const;
+		void incrementGrade();
+		void decrementGrade();
+
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+};
+
+std::ostream& operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
